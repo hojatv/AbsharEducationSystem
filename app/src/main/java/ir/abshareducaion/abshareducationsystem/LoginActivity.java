8c484618589f6276b13c8ff4951533ed94079051
hojatv <hojat.vaheb@gmail.com>
2016-09-07 00:12:28 +0800

    Login Form

--- app/src/main/java/ir/abshareducaion/abshareducationsystem/LoginActivity.java
+++ app/src/main/java/ir/abshareducaion/abshareducationsystem/LoginActivity.java
@@ -1,11 +1,14 @@
 package ir.abshareducaion.abshareducationsystem;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by hovaheb on 9/6/2016.
@@ -19,14 +22,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    public void onLoginClickHandler(View view){
-        if(view.getId() == R.id.btnLoggin){
-            Intent intent = new Intent(this,MainActivity.class);
+
+    public void onLoginClickHandler(View view) {
+        if (view.getId() == R.id.btnLoggin) {
+            Intent intent = new Intent(this, MainActivity.class);
             EditText userName = (EditText) findViewById(R.id.tfUserName);
             EditText password = (EditText) findViewById(R.id.tfPassword);
-            intent.putExtra(USER_NAME,userName.getText().toString());
-            intent.putExtra(PASSWORD,password.getText().toString());
-            startActivity(intent);
+            if (userName.getText().toString().equals("kermit") && password.getText().toString().equals("kermit")) {
+                intent.putExtra(USER_NAME, userName.getText().toString());
+                intent.putExtra(PASSWORD, password.getText().toString());
+                startActivity(intent);
+            }else{
+                Toast.makeText(this,"Please enter your login credentials",Toast.LENGTH_LONG).show();
+            }
         }
 
     }
